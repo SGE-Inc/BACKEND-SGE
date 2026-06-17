17c3498deb61f3089ada7b3d1d78ef17e470c036
João Tambue <tjoao7279@gmail.com>
2026-06-17 23:16:45 +0100

    added url to cors

--- src/main/providers/MiddlewaresProvider.ts
+++ src/main/providers/MiddlewaresProvider.ts
@@ -10,6 +10,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://127.0.0.1:3000",
+  "https://sistemadegestaoescolar.onrender.com",
   process.env.FRONTEND_URL,
 ].filter(Boolean) as string[];
 
